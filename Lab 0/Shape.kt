bca776c217c6992da3d5a6665f334fd56b9b39f5
Ziyike1 <111989865+Ziyike1@users.noreply.github.com>
2022-09-11 17:09:03 -0400

    Update Shape.kt

--- Lab 0/Shape.kt
+++ Lab 0/Shape.kt
@@ -1,5 +1,5 @@
-open class Shape(val name: String) : Dimensionable {
-
+abstract class Shape(_name: String) : Dimensionable {
+    var name = _name
     open fun getArea(): Double {
         return 0.0
     }
